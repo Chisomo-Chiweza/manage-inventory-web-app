ef348e1b32ec9a22bc31b874aed6228285aef256
Chisomo Chiweza <chisomochiweza@gmail.com>
2021-02-24 14:59:24 +0200

    I deleted some files to begin setting up my main project.

---
+++ server/resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.home')
+
+@section('content')
+
+<div class="bg-white float justify-center p-6 mt-10">
+    <h1>Welcome to the Inventory Management System</h1>
+</div>
+    
+@endsection
--- server/routes/web.php
+++ server/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
 
-Route::get('/home', function () {
-    return view('home.index');    
+Route::get('/', function () {
+       return view('welcome');
 });
