9a560714ffe1e37d03fcaf5f28895181b04ecca7
Chisomo Chiweza <chisomochiweza@gmail.com>
2021-02-25 15:12:49 +0200

    These are test routes

--- server/routes/web.php
+++ server/routes/web.php
@@ -6,3 +6,35 @@ use App\Http\Controllers\Auth\LoginController;
 Route::get('/', function () {
        return view('welcome');
 });
+
+Route::get('/welcomeadmin', function () {
+       return view('welcomeadmin');
+});
+
+Route::get('/user', function () {
+       return view('user');
+});
+
+Route::get('/inventory', function () {
+       return view('inventory');
+});
+
+Route::get('/admin', function () {
+       return view('admin');
+});
+
+Route::get('/transaction', function () {
+       return view('popups/transaction');
+});
+
+Route::get('/register', function () {
+       return view('popups/register');
+});
+
+Route::get('/delete', function () {
+       return view('popups/delete');
+});
+
+Route::get('/item', function () {
+       return view('items/item');
+});
