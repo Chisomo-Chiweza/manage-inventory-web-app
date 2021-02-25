5a3519a3497ea6d5f31951fac7f3e996f1e12f16
Chisomo Chiweza <chisomochiweza@gmail.com>
2021-02-25 10:15:08 +0200

    Created the page the user is directed to once logged in

---
+++ server/resources/views/user.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.dashboard')
+
+@section('content')
+
+<div class="flex justify-center pt-10">
+
+    <div class="bg-white p-4 rounded-lg w-10/12 mb-10">
+
+        <div class="text-gray-500 text-2xl text-center p-40">Chart</div>
+
+    </div>
+</div>
+
+<div class="flex justify-center">
+
+    <div>
+        <button class="bg-green-500 hover:bg-green-700 text-white px-4 py-3 rounded font-medium w-full">Create transaction</button>
+    </div>
+
+</div>
+
+@endsection
