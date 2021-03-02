8946fcfe81f8897aec98adaa4cae6c0bb3751838
Chisomo Chiweza <chisomochiweza@gmail.com>
2021-03-02 15:57:55 +0200

    The items weren't being stored properly, had to fix that for a while

--- server/app/Http/Controllers/TransactionController.php
+++ server/app/Http/Controllers/TransactionController.php
@@ -15,6 +15,7 @@ class TransactionController extends Controller
     public function store(Request $request) {
 
         $number = 1;
+        $check = true;
 
         // Form Validation
         $this->validate($request, [
@@ -25,52 +26,95 @@ class TransactionController extends Controller
             'quantity' => 'required'
         ]);
         
-
-        // Update already existing item
-        if ($request->user()->inventories()->where('name', $request->item)) {
-
-            // Increment value
-            if ($request->type == 'deposit') {
-                $users = $request->user()->inventories()->where('name', $request->item)->get();
-                    foreach ($users as  $user) {
-                        $number = (int) $user->number_of_items;
-                        $number += (int) $request->quantity;
-                    }
-                //Update record
-                $request->user()->inventories()->where('name', $request->item)->update(['number_of_items' => $number]);
-            } 
-            
-            // Withdraw value
-            if ($request->type == 'withdrawal') {
-                $users = $request->user()->inventories()->where('name', $request->item)->get();
-                    foreach ($users as  $user) {
-                        $number = (int) $user->number_of_items;
-                        $number -= (int) $request->quantity;
-                    }
-
-                //Update record
-                $request->user()->inventories()->where('name', $request->item)->update(['number_of_items' => $number]);
-            }
-
-        }  
+        $states = $request->user()->inventories()->get();
+        $isempty = $states->isEmpty();
         
-        // If item doesn't exist, create new one
-        if ($request->user()->inventories()->where('name', !($request->item))) {
+        if ($isempty) {
+
+            // Store first item
             $request->user()->inventories()->create([
                 'name' => $request->item,
                 'number_of_items' => $request->quantity,
             ]);
+
+            // Store first transaction
+            $request->user()->transactions()->create([
+                'date' => $request->date,
+                'initiated_by' => $request->initiator,
+                'type_of_transaction' => $request->type,
+                'item_name' => $request->item,
+                'qty' => $request->quantity,
+            ]);
+
+            return redirect()->route('user'); 
+
         }
 
-        // Store transaction
-        $request->user()->transactions()->create([
-            'date' => $request->date,
-            'initiated_by' => $request->initiator,
-            'type_of_transaction' => $request->type,
-            'item_name' => $request->item,
-            'qty' => $request->quantity,
-        ]);
+        foreach ($states as $state) {
 
-        return redirect()->route('user');
+            $check = (bool) ($state->name == $request->item);
+
+            if ($check) {
+
+                $number = 1;
+
+                // Increment value
+                if ($request->type == 'deposit') {
+                    $users = $request->user()->inventories()->where('name', $request->item)->get();
+                        foreach ($users as  $user) {
+                            $number = (int) $user->number_of_items;
+                            $number += (int) $request->quantity;
+                        }
+                    //Update record
+                    $request->user()->inventories()->where('name', $request->item)->update(['number_of_items' => $number]);
+                } 
+                
+                // Withdraw value
+                if ($request->type == 'withdrawal') {
+                    $users = $request->user()->inventories()->where('name', $request->item)->get();
+                        foreach ($users as  $user) {
+                            $number = (int) $user->number_of_items;
+                            $number -= (int) $request->quantity;
+                        }
+
+                    //Update record
+                    $request->user()->inventories()->where('name', $request->item)->update(['number_of_items' => $number]);
+                }
+
+                // Store transaction
+                $request->user()->transactions()->create([
+                    'date' => $request->date,
+                    'initiated_by' => $request->initiator,
+                    'type_of_transaction' => $request->type,
+                    'item_name' => $request->item,
+                    'qty' => $request->quantity,
+                ]);
+
+                return redirect()->route('user'); 
+
+            }
+                        
+            if (!$check) {
+
+                // Store item
+                $request->user()->inventories()->create([
+                    'name' => $request->item,
+                    'number_of_items' => $request->quantity,
+                ]);
+
+                // Store transaction
+                $request->user()->transactions()->create([
+                    'date' => $request->date,
+                    'initiated_by' => $request->initiator,
+                    'type_of_transaction' => $request->type,
+                    'item_name' => $request->item,
+                    'qty' => $request->quantity,
+                ]);
+
+                return redirect()->route('user'); 
+            }
+
+        }
+
     }
 }
