4fe1fd70d89a0f5a1e50e9e154b7d1921e5c42db
Chisomo Chiweza <chisomochiweza@gmail.com>
2021-03-03 06:16:06 +0200

    Successfully created charts for User Dashboard

--- server/resources/views/user.blade.php
+++ server/resources/views/user.blade.php
@@ -18,7 +18,16 @@
 
     <div class="bg-white p-4 rounded-lg w-10/12 mb-10">
 
-        <div class="text-gray-500 text-2xl text-center p-40">Chart</div>
+        @empty($name)
+            <canvas id="canvas" height="100" width="300" class="text-xl text-gray-600">
+                You currently have no items in your inventory
+            </canvas>
+        @endempty
+        
+        <canvas id="canvas" height="100" width="300">
+        
+        </canvas>
+        
 
     </div>
 </div>
@@ -30,5 +39,48 @@
     </div>
 
 </div>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+<script>
+
+    var ctx = document.getElementById("canvas").getContext('2d');
+    var ItemNames = {!! json_encode($name) !!};
+    var Numbers = {!! json_encode($number) !!};
+
+    var InventoryChart = new Chart(ctx, {
+
+        type: 'bar',
+
+        data: {
+
+            labels: ItemNames,
+            datasets: [{
+                label: 'Number of items',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: Numbers,
+                borderwidth: 1
+            }]
+            
+        },
+
+        options: {
+
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            },
+
+            title: {
+                display: true,
+                text: 'Items In Inventory'
+            }
+        }
+    });
+
+</script>
 
 @endsection
